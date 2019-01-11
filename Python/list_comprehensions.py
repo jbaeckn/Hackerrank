6fc62c642caa94f01a672efe1bcc9af3d1b1144b
jbaeckn <jhbaek94@gmail.com>
2019-01-11 21:08:55 +0900

    Basic Data Types

--- Python/list_comprehensions.py
+++ Python/list_comprehensions.py
@@ -4,5 +4,7 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 
-answer_list = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n]
+answer_list = [[i, j, k] for i in range(x+1) for j in range(y+1) \
+              for k in range(z+1) if (i+j+k) != n]
+
 print(answer_list)
